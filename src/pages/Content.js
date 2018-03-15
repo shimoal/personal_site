61c02d5ba95d21ab7aedadb649693592c80b2d5c
Alison Reed <alison.michelle.reed@gmail.com>
2018-03-14 19:17:43 -0700

    Add styling to content, increase content font size for testing

--- src/pages/Content.js
+++ src/pages/Content.js
@@ -4,10 +4,11 @@ import AboutPage from './AboutPage.js';
 import ContactPage from './ContactPage.js';
 import HomePage from './HomePage.js';
 import MyWorkPage from './MyWorkPage.js';
+import style from '../../styles/Content.css';
 
 export default function Content() {
 return (
-		<div>
+		<div className="content">
 			<HomePage />
 			<AboutPage />
 			<MyWorkPage />
