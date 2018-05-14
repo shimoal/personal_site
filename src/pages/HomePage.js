034594cd7040e400c33950fdc539130a22ddecd0
Alison Reed <alison.michelle.reed@gmail.com>
2018-05-13 17:38:43 -0700

    Add picture of self for home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import selfImageSource from '../../assets/self.jpg';
 
 export default function HomePage() {
 	return (
 		<div id="home">
 			<h1>Hello!</h1>
+			<img src={selfImageSource} />
 
 			<h2>My name is Alison, and I like making cool things</h2>
 		</div>
