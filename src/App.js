9b5e42339353fdb679fefef0be49133f31d70057
Alison Reed <alison.michelle.reed@gmail.com>
2018-06-08 20:41:02 -0700

    Fix import statement to take image from correct folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import backgroundPhoto from '../public/field.jpg';
+import backgroundPhoto from './images/field.jpg';
 import './App.css';
 
 class App extends Component {
