fa5630bcc104474fabe3cc108e1b1b6fc98247d3
Alison Reed <alison.michelle.reed@gmail.com>
2018-06-09 17:38:39 -0700

    Change style of background image for header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import backgroundPhoto from './images/field.jpg';
 import './styles/App.css';
 
 class App extends Component {
@@ -7,7 +6,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={backgroundPhoto} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to my page</h1>
         </header>
         <p className="App-intro">
