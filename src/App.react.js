bbc557b3b9092108bfb05551d520bfff5021bc93
Alison Reed <alison.michelle.reed@gmail.com>
2018-06-09 17:50:28 -0700

    Move landing page to its own component

--- src/App.react.js
+++ src/App.react.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+import LandingPage from './pages/LandingPage.react';
 import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to my page</h1>
-        </header>
+        <LandingPage />
         <p className="App-intro">
           Contact me at alison.michelle.reed@gmail.com
         </p>
---
+++ src/pages/LandingPage.react.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './../styles/LandingPage.css';
+
+const LandingPage = () => {
+  return (
+      <header className="LandingPage-section">
+        <h1 className="LandingPage-text">Welcome to my page</h1>
+      </header>
+    );
+}
+
+export default LandingPage;
