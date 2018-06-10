d7c1eaf78a8aed224966a79c8dbc9e7b881f80ea
Alison Reed <alison.michelle.reed@gmail.com>
2018-06-09 18:40:17 -0700

    Style landing page

--- src/pages/LandingPage.react.js
+++ src/pages/LandingPage.react.js
@@ -4,9 +4,9 @@ import './../styles/LandingPage.css';
 const LandingPage = () => {
   return (
       <header className="LandingPage-section">
-        <div>
+        <div className="LandingPage-textSection">
           <h1 className="LandingPage-title">Alison Reed</h1>
-          <h2 className="LandingPage-subtitle">Developer</h2>
+          <h2 className="LandingPage-subtitle">Web App Developer | San Francisco, CA</h2>
         </div>
       </header>
     );
